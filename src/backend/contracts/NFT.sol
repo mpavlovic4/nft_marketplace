73d7d93e7b8abcb767ee0fd5a7cf9d4490634f10
mpavlovic4 <m.pavlovic@emakina.rs>
2022-04-06 16:32:02 +0200

    Add NFT contract

---
+++ src/backend/contracts/NFT.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract NFT is ERC721URIStorage {
+    // Solidity will automatically initialize this state variable to default value for uint, which is 0
+    uint public tokenCount;
+
+    constructor() ERC721("DApp NFT", "DAAP") {}
+
+    function mint(string memory _tokenURI) external returns(uint) {
+        _safeMint(msg.sender, ++tokenCount);
+        _setTokenURI(tokenCount, _tokenURI);
+        return tokenCount;
+    }
+}
